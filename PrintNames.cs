ccbf11a0122fd8599ed4b112b5a96e0b7d7aa1d1
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 13:28:43 +0530

    program prints names

---
+++ PrintNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    public class PrintNames
+    {
+        public static void print()
+        {
+            string[] names = { "Aisha", "Vaishnav", "Bhavya", "Rutu", "Pragnya" };
+            Console.WriteLine("Names:");
+            foreach(string name in names)
+            {
+              
+                Console.Write(name+"  ");
+            }
+        }
+        
+        
+
+    }
+}
