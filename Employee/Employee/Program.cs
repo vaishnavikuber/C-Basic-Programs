b03a872c2ca627e1513e72c71c6306315340a282
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 12:02:22 +0530

    employee

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Employee
+{
+    internal class Program
+    {
+
+        static void checkAttendence()
+        {
+            Console.WriteLine("Employee Attendence");
+            Random random = new Random();
+            int employeePresent = 1;
+            int employeeInp = random.Next(0, 2);
+            if (employeeInp==employeePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            checkAttendence();
+
+        }
+    }
+}
