6191d98f8506099e61a0d28232bc5cc3c9603faa
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 16:33:27 +0530

    time taken by program

---
+++ ExecutionTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+
+namespace Programs
+{
+    internal class ExecutionTime
+    {
+
+        public static void executionTime()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Console.WriteLine("Hello");
+            Console.WriteLine("Here is the time!!!!");
+            watch.Stop();
+            TimeSpan span = watch.Elapsed;
+
+            Console.WriteLine("Execution time is : "+span.TotalSeconds+" s");
+        }
+
+    }
+}
