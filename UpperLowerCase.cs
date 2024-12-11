073797b1ec3d46a1a98920763b9dc85df0625b8c
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 15:59:17 +0530

    string

---
+++ UpperLowerCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class UpperLowerCase
+    {
+
+        public static void lowerUpper()
+        {
+            Console.WriteLine("Enter the place name:");
+            string placeName=Console.ReadLine();
+            string lower = placeName.ToLower();
+            string upper = placeName.ToUpper();
+            Console.WriteLine("Upper case: "+upper);
+            Console.WriteLine("Lower case: "+lower);
+
+        }
+
+    }
+}
