677156ada9456d8d05f23588aa7584766fc4bd47
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 15:43:20 +0530

    circle

---
+++ AreaAndPerimeterOfCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Programs
+{
+    internal class AreaAndPerimeterOfCircle
+    {
+        const double pi = Math.PI;
+        public static void areaAndPerimeter()
+        {
+            
+            Console.WriteLine("Enter the diameter of circle");
+            double diameter = double.Parse(Console.ReadLine());
+            double radius = diameter / 2;
+            double area = 2*pi*pi;
+            double perimiter = 2 * pi * radius;
+            Console.WriteLine("Area of circle is: "+area);
+            Console.WriteLine("Perimeter of circle is : "+perimiter);
+        }
+              
+    }
+}
