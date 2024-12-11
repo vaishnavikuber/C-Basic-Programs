7062c4ffaa6435d4f35d051887944a927f0025ef
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 16:32:44 +0530

    square and difference

---
+++ SquareAndDifferenceOfTwoNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    public class SquareAndDifferenceOfTwoNumbers
+    {
+
+        public static void squareAndDifference(double a,double b)
+        {
+            double sumOfSquareOfNumbers = sumSquare(a,b);
+            Console.WriteLine("Sum of square of number is: "+sumOfSquareOfNumbers);
+            double diffOfSquareOfNumbers = diffSquare(a, b);
+            Console.WriteLine("Difference of square of number is: " + diffOfSquareOfNumbers);
+        }
+        static double sumSquare(double a,double b)
+        {
+            double num = a + b;
+            return num*num;
+        }
+        static double diffSquare(double a, double b)
+        {
+            double num = a - b;
+            return num*num;
+        }
+    }
+}
