b8fb6bece16285754d8354848aace688fd39785d
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 12:26:29 +0530

    C# Programs

---
+++ BasicPrograms/Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
