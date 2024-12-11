fdf1b9f52c01c8e2018dc7cffaf01c9d21afeade
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 15:57:57 +0530

    parts of string

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class Person
+    {
+
+        public static void printName()
+        {
+            Console.WriteLine("Enter first name ,middle name, last name:");
+            string name= Console.ReadLine();
+
+            int lenght=name.Split().Length;
+
+            Console.WriteLine("Number of parts: "+lenght);
+            Console.WriteLine("name: "+name);
+
+        }
+
+    }
+}
