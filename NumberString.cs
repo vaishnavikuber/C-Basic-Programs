483c3e49fb8932b9477d0329abed7316455b7b2f
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 15:56:27 +0530

    strings

---
+++ NumberString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class NumberString
+    {
+        public static void numstring()
+        {
+            Console.WriteLine("Enter the number");
+            string num = Console.ReadLine();
+            for (int i = 1; i <=10 ; i++)
+            {
+                Console.Write(num+" ");
+            }
+        }
+
+    }
+}
