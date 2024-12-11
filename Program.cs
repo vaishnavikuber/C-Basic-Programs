0c60557d8ad9f0aae0a55d46e746f0c630aef584
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-11 15:58:35 +0530

    function calls

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintNames.print();
+
+            //Console.WriteLine("Enter two numbers");
+            //double a = double.Parse(Console.ReadLine());
+            //double b = double.Parse(Console.ReadLine());
+            //SquareAndDifferenceOfTwoNumbers.squareAndDifference(a,b);
+
+            //UpperLowerCase.lowerUpper();
+
+            //NumberString.numstring();
+
+            //Person.printName();
+
+            //AverageOfRandomNumbers.averageRandom();
+
+            //AreaAndPerimeterOfCircle.areaAndPerimeter();
+
+            DivisibleBY7ButNotMultipleOf5.method();
+
+            Console.WriteLine();
+        }
+    }
+}
