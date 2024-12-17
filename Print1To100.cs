85919ff8eb50275a56b58bc2851a3f601bf66268
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 15:16:14 +0530

    prints 1 to 100 with recursion

---
+++ Print1To100.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class Print1To100
+    {
+
+        
+        public static void PrintNum(int n)
+        {
+            if(n> 0) { 
+
+                PrintNum(n - 1);
+                Console.Write(n + "  ");
+            }
+        }
+    }
+}
