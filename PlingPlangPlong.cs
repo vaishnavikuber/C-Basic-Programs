ab55a3aab1a059117dc88caa5401cc21c6cc5d99
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 16:06:11 +0530

    divisible by 3 5 7

---
+++ PlingPlangPlong.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class PlingPlangPlong
+    {
+
+        public static void Divisible(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0)
+            {
+                Console.WriteLine("Pling  Plang Plong");
+            }
+            else if(num% 3==0 && num % 5 == 0)
+            {
+                Console.WriteLine("Pling  Plang");
+            }
+            else if (num % 3 == 0 && num % 7 == 0)
+            {
+                Console.WriteLine("Pling  Plong");
+            }
+            else if (num % 7 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("Plang  Plong");
+            }
+
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Pling");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Plang");
+            }
+            else if (num % 7 == 0)
+            {
+                Console.WriteLine("Plong");
+            }
+
+        }
+    }
+}
