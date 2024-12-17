221ba08aead95623df4eaa37fa94ca50af5e97f9
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 16:48:25 +0530

    to return a prime number among them

---
+++ PrimeNumberInArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class PrimeNumberInArray
+    {
+
+        public static int Prime(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 2;
+                if (nums[i] % 2 == 0)
+                {
+                    count++;
+                }
+                if(count==2)
+                    return nums[i];
+            }
+            return -1;
+        }
+    }
+}
