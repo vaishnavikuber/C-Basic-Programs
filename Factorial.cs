7a358185b55b8e8353b92f06b098fb462b9f3a66
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 14:57:07 +0530

    factorial of a number

---
+++ Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class Factorial
+    {
+
+        public static int FactorialOfNumber(int num)
+        {
+            int fact = 1;
+
+            do
+            {
+                fact = fact* num;
+                num = num - 1;
+
+            }while (num > 0);
+
+            return fact;
+        }
+
+
+    }
+}
