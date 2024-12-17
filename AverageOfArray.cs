b711b7b65aa9fc9a778fb47a26a1ba26c4747e17
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 16:33:46 +0530

    average of double array

---
+++ AverageOfArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    internal class AverageOfArray
+    {
+        public static double Average(double[] array)
+        {
+            double total = 0;
+            for(int i=0; i<array.Length; i++)
+            {
+                total += array[i];
+            }
+            return total/array.Length;
+        }
+
+    }
+}
